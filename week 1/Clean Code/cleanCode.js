d00e4c6d73c6a3119f7bcc745fb0221af74b3ec4
evangel <evangeldaping@gmail.com>
2021-03-09 13:07:18 +0800

    Clean Code

---
+++ week 1/Clean Code/cleanCode.js
@@ -0,0 +1,39 @@
+console.log("Example 1")
+
+function sumAll(a, b){
+    var c =a+b
+    return c;
+}
+
+console.log("Example 2")
+
+function abc(a,b,c,d, e){
+    var x = a+b;
+    var y = c+2;
+    var z = d-e;
+
+    return x-y+z; 
+}
+    abc(3,5,10,3,7);
+
+console.log("Example 3")
+    
+function abc(a,b,c,d,e){
+    for(var i=0; i<a; i++){
+        b += i;
+    }  
+    return b+d; 
+}
+    abc(1,2,3,4,5);
+
+console.log("Example 4")
+
+function compare(a,b){
+    if(a<b){
+        return b
+    }
+    else{ 
+        return a;   
+    } 
+}
+    compare(2,3);

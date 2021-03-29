91eac91a1b77a2fc64ab0b06ada0ff854e3443a0
evangel <evangeldaping@gmail.com>
2021-03-29 19:49:28 +0800

    Functions - Updated

--- week 4/PHP/Functions/index.php
+++ week 4/PHP/Functions/index.php
@@ -118,4 +118,28 @@ print_array($samples);
 
 $samples  = array(13, 52, 22);
 print_array($samples);
+
+function car_factory($raw_materials)
+{
+    $cars = array();
+    foreach ($raw_materials as $raw_material) {
+        $cars[] = "<p>Just produced a {$raw_material['paint']} car that used {$raw_material['metal']} for the body and {$raw_material['rubber']} for the car's tires. </p>";
+    }
+    return $cars;
+}
+$raw_materials = array(
+    array(
+        'rubber' => 'Natural Rubber',
+        'metal' => 'Alloy',
+        'paint' => 'Green'
+    ),
+    array(
+        'rubber' => 'Nitrile',
+        'metal' => 'Brass',
+        'paint' => 'Red'
+    )
+);
+$cars = car_factory($raw_materials);
+var_dump($cars);
+
 ?>

ee5363a184a83de35a8852a68e6b2575e37bf340
evangel <evangeldaping@gmail.com>
2021-03-29 19:34:47 +0800

    Built-in Functions

---
+++ week 4/PHP/Built-in Functions/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Built-in Functions</title>
+</head>
+
+<body>
+    <main>
+        <!-- PHP has built-in functions that are very useful for accomplishing certain tasks. Each function has a specified number of parameters, sometimes none at all, and will return a value.
+
+        You don't have to memorize all of these. It is more important for you to be able to reference any function and use it than to have a group of them memorized. You will learn more about functions as you continue to develop in PHP.
+
+        Functions for Strings
+        strlen - get string length
+        strtolower - make a string lowercase
+        strtoupper - make a string uppercase
+        ucwords - uppercase the first character of each word in a string
+        str_replace - replace all occurrences of the search string with the replacement string
+        stristr - find the first occurrence of a string
+        strpos - find the position of the first occurrence of a substring in a string
+        explode - split a string
+        implode - joins arrays to a string
+        Reference: http://php.net/manual/en/ref.strings.php
+
+        Functions for Arrays
+        shuffle - shuffles the array
+        sort - sorts the array
+        count - counts the number of rows in the array
+        explode - breaks string into multiple values in the array
+        implode - joins information in the array as a string
+        array_pop - remove the last element in an array
+        array_push - adding elements into an array
+        Reference: http://php.net/manual/en/ref.array.php
+
+        Functions for Numbers
+        is_numeric(num) - returns true or false depending on whether or not the value is a number
+        rand(start, end) - generates a random number
+        Additional Functions
+        isset(var) - returns true if var is set and is not NULL
+        empty(var) - Returns FALSE if var exists and has a non-empty, non-zero value. Otherwise returns TRUE. -->
+
+    </main>
+</body>
+
+</html>

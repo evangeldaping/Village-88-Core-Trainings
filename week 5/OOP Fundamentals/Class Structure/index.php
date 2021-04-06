acc8ccaad8dca9c85a0563aa1ba6433621f3a58a
evangel <evangeldaping@gmail.com>
2021-04-06 14:30:57 +0800

    Class Structure

---
+++ week 5/OOP Fundamentals/Class Structure/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Structure</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<?php
+//Instantiation
+class MyFirstClass
+{
+}
+
+$obj = new MyFirstClass();
+$obj1 = new MyFirstClass();
+$obj2 = new MyFirstClass();
+$obj3 = new MyFirstClass();
+$obj4 = new MyFirstClass();
+
+var_dump($obj);
+var_dump($obj1);
+var_dump($obj2);
+var_dump($obj3);
+var_dump($obj4);

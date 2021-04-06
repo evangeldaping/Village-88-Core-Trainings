5d10914e22ad7ce3b30fcc43d7deb90166528d79
evangel <evangeldaping@gmail.com>
2021-04-06 22:31:09 +0800

    Procedural vs OOP

---
+++ week 5/OOP Advanced/Procedural vs OOP/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procedural vs OOP</title>
+</head>
+
+<body>
+    <!-- Procedural vs. OOP
+    In this tab, we'll talk about some of the benefits of OOP vs Procedural Programming.
+
+    Benefits of OOP
+    A lot of people ask, why use OOP when using OOP takes much longer? It's true that OOP takes much longer IF your project is really small. However, as your project gets bigger and bigger, OOP will save you hours/weeks/months of development time. Following are some key benefits of using Object Oriented Programming.
+
+    1. It allows you to leverage other people's codes (libraries, APIs, frameworks)
+
+    Remember that a class is just a collection of functions and attributes and serves as a blueprint. Let's say that you are writing a library with a lot of useful functions you need when creating lots of web applications. Let's say that you've built so many web applications that you know what functions/methods other developers would need and you decided to, therefore, create a framework (basically, a fancy word for a bunch of classes) with built-in functions for other developers to use.
+
+    With OOP, you can create several classes for other developers to inherit and use. Since all of your functions/methods are contained in your own classes, other developers do not even need to modify your class/functions to use your functions. They can simply create their own class and have it inherit some of the classes you wrote (and thereby gaining access to all of the cool functions you built)! This saves a lot of time and allows you to leverage other people's codes.
+
+    2. It allows you to maintain and organize complex projects into more manageable chunks
+
+    Just think of it this way. If you had 5 files in your folder, is there any need to create multiple folders? Probably not. What if you have 1000 files? Or 100,000 files? As the number of files gets larger and larger, wouldn't it help to organize your folders in different folders that do specific things (e.g. photos, wall, etc)? Objects/Classes are exactly like folders in this scenario. Doing things the OOP way takes a little longer when the project is small (when you only have 5 files in the folder) but when the project gets bigger, OOP helps you to organize your codes more neatly.
+
+    When Not to Use OOP
+    Use the DRY(Don't Repeat Yourself) principle. Not everything should be Object Oriented. When dealing with a stand alone function that serves a specific purpose, there is no need to create a class. Reserve using OOP when you notice yourself using a series of functions that work together for one main output or cause. -->
+</body>
+
+</html>

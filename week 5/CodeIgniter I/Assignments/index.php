0c7c65f56d3ef21d7893cd163217b0dc3c546950
evangel <evangeldaping@gmail.com>
2021-04-08 23:39:43 +0800

    Assignments

---
+++ week 5/CodeIgniter I/Assignments/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignments</title>
+</head>
+
+<body>
+    <!-- Assignments Introduction
+    The goals of the assignments in this chapter are to:
+
+    help you understand how to use Session and Post using CodeIgniter's built in libraries.
+    get you familiar with the use of routing file
+    get you comfortable setting up controllers, have it work with view files and different models
+    (optional) get you to understand ORM (Object Relational Mapper) and see its benefits and perhaps disadvantages
+    Again, it's extremely important you know how to use POST and SESSION as these are CRITICAL in building any web applications. Other things you'll later learn in this track, such as AJAX and APIs are a lot easier to learn if you mastered these concepts.
+
+    The ultimate goal is to teach these fundamentals so that you can become a self-sufficient programmer who knows how to use an MVC framework! The enterprise level web applications are almost always built with an MVC framework and we believe these assignments will help you understand how MVC works. There will, of course, be materials within CodeIgniter that we haven't taught but based on our years of work, building many enterprise level apps, we believe the fundamentals we teach in this chapter and the principles you'll be learning by doing the assignments in this chapter, will help you to build 90-95% of whatever you're asked to build and hopefully by doing these assignments, you'll know how to learn something new even if it's something you may not have learned in this particular chapter. After all, best developers are those that know how to learn something new quickly and efficiently. Have fun with these assignments!
+
+    Tips (this chapter is one of the most important chapters for the entire bootcamp)
+    Do NOT start any of these assignments UNTIL you have completed all the quizzes and understand all the key concepts in the quiz. Doing the quiz first and then doing the assignments will save a lot of your time.
+    If you're attending the on-site bootcamp, please make sure you work on the assignments with someone else. By now, you should know who you feel comfortable working with. Find a good pair programmer or two to work with, and together do these assignments. Know that pair programming increases your programming speed by almost a factor of 2, which means if you do these assignments with someone else, not only will you complete the assignments about twice as fast but also you'll have a lot more fun to work with someone else and have someone to share the victories as well as struggles than doing all of these individually. Again, the best developers that come out of the bootcamp are those that take the time to help others and those that do the projects/assignments with another programmer. -->
+</body>
+
+</html>

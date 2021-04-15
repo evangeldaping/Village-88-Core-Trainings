1294e0037d11a21c3579dee63f3dd6939a5ce49c
evangel <evangeldaping@gmail.com>
2021-04-15 11:29:19 +0800

    XSS Filtering and CSRF - updated

--- week 6/CodeIgniter II/XSS filtering and CSRF/index.php
+++ week 6/CodeIgniter II/XSS filtering and CSRF/index.php
@@ -9,7 +9,14 @@
 </head>
 
 <body>
+    <!-- XSS Filtering and CSRF
+    Cross-site scripting (XSS) is a type of security vulnerability found in a lot of web applications. Spend up to 30 minutes to learn more about this including going through https://en.wikipedia.org/wiki/Cross-site_scripting.
 
+    Cross-site request forgery (CSRF) is another common security vulnerability found in a lot of web applications. Spend up to 30 minutes to learn more about this including going through https://en.wikipedia.org/wiki/Cross-site_request_forgery.
+
+    Now, most frameworks have tools to prevent your site from being attacked with CSS and CSRF. For CodeIgniter, see how this is done by reading: https://codeigniter.com/userguide3/libraries/security.html
+
+    Going forward, for all CodeIgniter projects, make sure your application is secure from these two common attacks. -->
 </body>
 
 </html>

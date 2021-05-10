3c52c95bc42efb1b7ac00c3bcffc33ef2166d6ad
evangel <evangeldaping@gmail.com>
2021-05-10 10:24:20 +0800

    JS Object I

---
+++ week 10/Javascript/Hacker Hero Challenges/Hacker Hero Adv Algorithm Course/index.js
@@ -0,0 +1,17 @@
+function displayNameCountry(lists){
+    for (var i = 0; i < lists.length; i++) {
+        console.log("Name: " + lists[i].name + ", Cohort: " + lists[i].country)
+    }
+}
+
+let students  = [
+    {name: 'Remy', country: 'Korea'},
+    {name: 'Chuck', country: 'China'},
+    {name: 'Osmund', country: 'U.S.A.'},
+    {name: 'Nicole', country: 'South Africa'},
+    {name: 'Boris', country: 'France'},
+    {name: 'John', country: 'Philippines'},
+    {name: 'Michael', country: 'Ukraine'}
+];
+
+displayNameCountry(students);
